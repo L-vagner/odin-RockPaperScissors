7d4a61ddddaf59844a7fdec80b4cafcd735252f5
Ludovic Vagner <ludovicvagner@yahoo.fr>
2024-10-31 11:38:19 +0100

    Ajout fonction choix

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Hello World!")
+
+function throwHand(string){
+    let hand;
+    let handVal;
+    
+    if (typeof string !== typeof "a")
+    {
+        console.error("La valeur passé n'est pas une chaine de charatères")
+        return false;
+    }
+    hand = string.toLowerCase()
+
+    switch (hand){
+
+        case "r":
+        case "rock":
+            handVal = 1;
+            break;
+
+        case "p":
+        case "paper":
+            handVal = 2;
+            break;
+        
+        case "s":
+        case "scissors":
+            handVal = 3;
+
+    }
+
+    return handVal;
+}
